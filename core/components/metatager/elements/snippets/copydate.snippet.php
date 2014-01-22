aaeb682b605cf1439e4b1a1fb288a240943d398b
ILYA ERSHOV <ershov.ilya@gmail.com>
2014-01-22 00:52:11 -0800

    Create copydate.snippet.php

---
+++ core/components/metatager/elements/snippets/copydate.snippet.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Snippet: CopyDate
+ * File:    copydate.snippet.php
+ * Date: 22.01.14, time: 12:15
+ * Author:  ILYA ERSHOV
+ * http://about.me/ershov.ilya
+ * GitHub:  https://github.com/ershov-ilya
+ * Edited in PhpStorm.
+ *
+ * Outputs string like, with current year: © 2014
+ */
+
+return "&copy; ".gmdate("Y");
